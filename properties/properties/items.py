66fb0d380fc55595a696281928c56c37a77d3d3e
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2020-01-26 01:46:27 +0300

    some scrapy code

--- properties/properties/items.py
+++ properties/properties/items.py
@@ -5,10 +5,24 @@
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
 
-import scrapy
+from scrapy.item import Item, Field
 
 
-class PropertiesItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class PropertiesItem(Item):
+    # Primary fields
+    title = Field()
+    price = Field()
+    description = Field()
+    address = Field()
+    image_urls = Field()
+
+    # Calculated fields
+    images = Field()
+    location = Field()
+
+    # Housekeeping fields
+    url = Field()
+    project = Field()
+    spider = Field()
+    server = Field()
+    date = Field()
