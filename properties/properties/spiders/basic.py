d2623c87aebb7810225ad4a57d61a712a620ee3d
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2020-02-18 23:24:58 +0300

    with loaders

--- properties/properties/spiders/basic.py
+++ properties/properties/spiders/basic.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
+import datetime
+import socket
+from urllib.parse import urlparse, urljoin
 from scrapy import Spider
 from scrapy.loader import ItemLoader
+from scrapy.loader.processors import MapCompose, Join
 from properties.items import PropertiesItem
 
 
@@ -10,13 +14,26 @@ class BasicSpider(Spider):
     start_urls = ['http://localhost:9312/properties/property_000000.html']
 
     def parse(self, response):
-        l = ItemLoader(item=PropertiesItem(), response=response)
+        loader = ItemLoader(item=PropertiesItem(), response=response)
 
-        l.add_xpath('title', '//*[@itemprop="name"][1]/text()')
-        l.add_xpath('price', '//*[@itemprop="price"][1]/text()', re='[.0-9]+')
-        l.add_xpath('description', '//*[@itemprop="description"][1]/text()')
-        l.add_xpath('address', '//*[@itemtype='
-                    '"http://schema.org/Place"][1]/text()')
-        l.add_xpath('image_urls', '//*[@itemprop="image"][1]/@src')
+        loader.add_xpath('title', '//*[@itemprop="name"][1]/text()',
+                         MapCompose(str.strip, str.title))
+        loader.add_xpath('price', '//*[@itemprop="price"][1]/text()',
+                         MapCompose(lambda i: i.replace(',', ''), float),
+                         re='[.0-9]+')
+        loader.add_xpath('description',
+                         '//*[@itemprop="description"][1]/text()',
+                         MapCompose(str.strip, Join()))
+        loader.add_xpath('address', '//*[@itemtype='
+                         '"http://schema.org/Place"][1]/text()',
+                         MapCompose(str.strip))
+        loader.add_xpath('image_urls', '//*[@itemprop="image"][1]/@src',
+                         MapCompose(lambda i: urljoin(response.url, i)))
 
-        return l.load_item()
+        loader.add_value('url', response.url)
+        loader.add_value('project', self.settings.get('BOT_NAME'))
+        loader.add_value('spider', self.name)
+        loader.add_value('server', socket.gethostname())
+        loader.add_value('date', datetime.datetime.now())
+
+        return loader.load_item()
