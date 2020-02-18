3771f3992cf63394353d0f307b26aec4bc9f67e8
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2020-02-18 18:08:58 +0300

    the first version of items.py (#6)

--- properties/properties/items.py
+++ properties/properties/items.py
@@ -5,10 +5,31 @@
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
 
-import scrapy
+from scrapy import Item, Field
 
 
-class PropertiesItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class PropertiesItem(Item):
+    # Primary fields
+    title = Field()
+    price = Field()
+    description = Field()
+    address = Field()
+    image_urls = Field()
+
+    # Calculated fields
+    images = Field()
+    location = Field()
+
+    # Housekeeping fields
+    url = Field()
+    project = Field()
+    spider = Field()
+    server = Field()
+    date = Field()
+    '''
+    response.url
+    self.settings.get('BOT_NAME')
+    self.name
+    socket.gethostname()
+    datetime.datetime.now()
+    '''
