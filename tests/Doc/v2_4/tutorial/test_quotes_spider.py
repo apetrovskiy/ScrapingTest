448975be8a3995f75a372377f2aeacac8510f0c0
Alexander-Petrovskiy <apetrovskiy2@hotmail.com>
2021-03-08 23:12:03 +0300

    Update test_quotes_spider.py

--- tests/Doc/v2_4/tutorial/test_quotes_spider.py
+++ tests/Doc/v2_4/tutorial/test_quotes_spider.py
@@ -1,6 +1,7 @@
 import pytest
 from pytest_mock import MockerFixture
-from scrapy.http.response import Response
+# from scrapy.http.response import Response
+from scrapy.http.response.html import HtmlResponse
 from Doc.v2_4.tutorial.tutorial.spiders.quotes_spider import QuotesSpider
 
 
@@ -19,16 +20,26 @@ def new_rsplit(data: str, number: int):
 
 
 def test_quotes_spider(mocker: MockerFixture) -> None:
+    # v 1
     # response = mocker.patch(Response)
     # response.url = 'http://localhost/1/'
     # response.body = 'body data'
     # response.rsplit = ""
-    with mocker.patch.object(Response, 'rsplit', new=new_rsplit):
-        obj = Response()
-        obj.url = 'http://localhost/1/'
-        obj.body = 'body data'
-        spider = QuotesSpiderChild(obj.body)
-        spider.parse(obj)
     # spider = QuotesSpiderChild(response.body)
     # spider.parse(response)
+
+    # v 2
+    # with mocker.patch.object(Response, 'rsplit', new=new_rsplit):
+    #     obj = Response()
+    #     obj.url = 'http://localhost/1/'
+    #     obj.body = 'body data'
+    #     spider = QuotesSpiderChild(obj.body)
+    #     spider.parse(obj)
+
+    url = 'http://localhost/1/'
+    body = b"body data"
+    response = HtmlResponse(url=url, body=body)
+    spider = QuotesSpiderChild(response.body)
+    spider.parse(response)
+
     # spider.save_file("filename", response.body)
