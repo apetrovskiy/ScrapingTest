8ade95ad971d9724bace0912d5d0cf741357f816
Alexander-Petrovskiy <apetrovskiy2@hotmail.com>
2021-03-13 23:26:24 +0300

    next changes

--- Hajba/ch04/sainsburys/sainsburys/spiders/basic.py
+++ Hajba/ch04/sainsburys/sainsburys/spiders/basic.py
@@ -1,10 +1,19 @@
 import scrapy
+# from scrapy import Request
 
 
 class BasicSpider(scrapy.Spider):
     name = 'basic'
-    allowed_domains = ['https://www.sainsburys.co.uk/shop/gb/groceries/meat-fish']
-    start_urls = ['http://https://www.sainsburys.co.uk/shop/gb/groceries/meat-fish/']
+    # allowed_domains = [
+    #     'https://www.sainsburys.co.uk/shop/gb/groceries/meat-fish']
+    allowed_domains = ['www.sainsburys.co.uk']
+    start_urls = ['https://www.sainsburys.co.uk/shop/gb/groceries/meat-fish/']
 
     def parse(self, response):
-        pass
+        # urls = response.xpath(
+        #     '//ul[@class="categories departments"]/li/a/@href').extract()
+        urls = response.xpath('//ul[@class="categories departments"]/li/a')
+
+        for url in urls:
+            # yield Request(url, callback=self.parse_department_pages)
+            yield response.follow(url, callback=self.parse_department_pages)
