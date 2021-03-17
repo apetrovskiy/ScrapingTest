8f4e849d867efd0979b59c0f6d294cced30ee91b
Alexander-Petrovskiy <apetrovskiy2@hotmail.com>
2021-03-17 16:40:44 +0300

    next changes

--- Hajba/ch04/sainsburys/sainsburys/spiders/basic.py
+++ Hajba/ch04/sainsburys/sainsburys/spiders/basic.py
@@ -2,7 +2,7 @@ import re
 # import scrapy
 from scrapy.http import HtmlResponse
 from scrapy.spiders import Spider
-from Hajba.ch04.sainsburys.sainsburys.items import SainsburysItem
+from sainsburys.items import SainsburysItem
 
 
 reviews_pattern = re.compile("Reviews \((\d+)\)")
@@ -52,37 +52,41 @@ class BasicSpider(Spider):
             yield response.follow(next_page, callback=self.handle_product_listings)
 
     def parse_product_detail(self, response: HtmlResponse):
-        item=SainsburysItem()
-        item['url']=response.url
-        # TODO: continue
-        product_name = response.xpath('//h1/text()').extract()[0].strip()
+        item = SainsburysItem()
+        item['url'] = response.url
+        # item['product_name'] = response.xpath('//h1/text()').extract()[0].strip()
+        item['product_name'] = "" if response.xpath('//h1/text()').extract() == [] else response.xpath('//h1/text()').extract()[0].strip()
         # product_image = response.urljoin(response.xpath('//div[@id="productImageHolder"]/img/@src').extract()[0])
         # product_image = response.urljoin(response.xpath('//div[@class="pd__left skipto-content"]/img/@src').extract()[0])
         # product_image = response.urljoin(response.xpath('//div[@class="productInfo"]//img/@src').extract()[0])
-        product_image = response.urljoin(response.xpath('//div[@class="pd__left skipto-content"]//img/@src').extract()[0])
+        item['product_image'] = "" if response.xpath('//div[@class="pd__left skipto-content"]//img/@src').extract() == "" else response.urljoin(response.xpath('//div[@class="pd__left skipto-content"]//img/@src').extract()[0])
 
         # price_per_unit = response.xpath('//div[@class="pricing"]/p[@class="pricePerUnit"]/text()').extract()[0].strip()
-        price_per_unit = response.xpath('//div[@class="pd__price-wrapper"]//div[@class="pd__cost__total undefined"]/text()').extract()[0].strip()
+        item['price_per_unit'] = response.xpath(
+            '//div[@class="pd__price-wrapper"]//div[@class="pd__cost__total undefined"]/text()').extract()[0].strip()
         # units = response.xpath('//div[@class="pricing"]/span[@class="pricePerUnitUnit"]').extract()
         # units = response.xpath('//div[@class="pricing"]//span[@class="pricePerUnitUnit"]').extract()
-        units = response.xpath('//div[@class="pd__price-wrapper"]//span[@class="pd__cost__per-unit"]').extract()
+        units = response.xpath(
+            '//div[@class="pd__price-wrapper"]//span[@class="pd__cost__per-unit"]').extract()
         if units:
-            unit = units[0].strip()
+            item['unit'] = units[0].strip()
 
         # ratings = response.xpath('//label[@class="numberOfReviews"]/img/@alt').extract()
         # ratings = response.xpath('//a[@class="numberOfReviews"]/img/@alt').extract()
         ratings = response.xpath('//div[@class="star-rating"]').extract()
         if ratings:
-            rating = ratings[0]
+            item['rating'] = ratings[0]
         # reviews = response.xpath('//label[@class="numberOfReviews"]').extract()
-        reviews = response.xpath('//a[@class="star-rating-link"]/span[@class="pd__reviews__read"]').extract()
+        reviews = response.xpath(
+            '//a[@class="star-rating-link"]/span[@class="pd__reviews__read"]').extract()
         if reviews:
             reviews = reviews_pattern.findall((reviews[0]))
             if reviews:
-                product_reviews = reviews[0]
+                item['product_reviews'] = reviews[0]
 
         # item_code = item_code_pattern.findall(response.xpath('//p[@class="itemCode"]/text()').extract()[0].strip())[0]
-        item_code = item_code_pattern.findall(response.xpath('//span[@id="productSKU"]/text()').extract()[0].strip())[0]
+        item['item_code'] = item_code_pattern.findall(response.xpath(
+            '//span[@id="productSKU"]/text()').extract()[0].strip())[0]
 
         nutritions = {}
         # for row in response.xpath('//table[@class="nutritionTable"]/tr'):
@@ -92,6 +96,9 @@ class BasicSpider(Spider):
                 th = ['Energy kcal']
             td = row.xpath('./td[1]/text()').extract()[0]
             nutritions[th[0]] = td
+        item['nutritions'] = nutritions
 
-        product_origin = ' '.join(response.xpath(
+        item['product_origin'] = ' '.join(response.xpath(
             './/h3[@class="productDataItemHeader" and text()="Country of Origin"]/following-sibling::div[1]/p/text()').extract())
+
+        yield item
